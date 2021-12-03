bd3a5092ab8020071dc86921570ca07832366df7
Junkeun Yi <junkeun.yi@berkeley.edu>
2021-12-03 00:02:04 -0800

    add new requirements and gitignore

--- cs285/policies/teacher_policy.py
+++ cs285/policies/teacher_policy.py
@@ -106,10 +106,11 @@ class DistillationTeacherPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     # Return the action distribution of the teacher policy on the given observation
-    def forward(self, observations):
+    def forward(self, observation: torch.FloatTensor)
+        obs = observation
 
         action_dist = self.model.policy.get_distribution(
-            self.model.policy.obs_to_tensor(observations)[0])
+            self.model.policy.obs_to_tensor(obs)[0])
         return action_dist
 
     ####################################
