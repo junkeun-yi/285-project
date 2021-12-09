79bb006f7f23314872908cb764fe5e1898d77992
Paul Ngo <paulngo123@gmail.com>
2021-12-08 16:04:20 -0800

    update ablations commands

--- distillablations.sh
+++ distillablations.sh
@@ -1,13 +1,13 @@
 MsPacman
 bad:
-python cs285/scripts/run_distillation.py --env MsPacmanNoFrameskip-v4 --teacher_chkpt cs285/teachers/envMsPacmanNoFrameskip-v4_20211206-021910_n_iters100000 --no_gpu
+python cs285/scripts/run_distillation.py --env MsPacmanNoFrameskip-v4 --teacher_chkpt cs285/teachers/envMsPacmanNoFrameskip-v4_20211206-021910_n_iters2000000
 
 best:
-python cs285/scripts/run_distillation.py --env MsPacmanNoFrameskip-v4 --teacher_chkpt cs285/teachers/envMsPacmanNoFrameskip-v4_20211206-021910_n_iters40000000.zip --no_gpu
+python cs285/scripts/run_distillation.py --env MsPacmanNoFrameskip-v4 --teacher_chkpt cs285/teachers/envMsPacmanNoFrameskip-v4_20211206-021910_n_iters40000000
 
 QuBert
 bad:
-python cs285/scripts/run_distillation.py --env QbertNoFrameskip-v4 --teacher_chkpt cs285/teachers/envPongNoFrameskip-v4_20211206-021704_n_iters100000.zip --no_gpu
+python cs285/scripts/run_distillation.py --env QbertNoFrameskip-v4 --teacher_chkpt cs285/teachers/envQbertNoFrameskip-v4_20211206-021759_n_iters1000000
 
 best:
-python cs285/scripts/run_distillation.py --env QbertNoFrameskip-v4 --teacher_chkpt cs285/teachers/envPongNoFrameskip-v4_20211206-021704_n_iters40000000.zip --no_gpu
+python cs285/scripts/run_distillation.py --env QbertNoFrameskip-v4 --teacher_chkpt cs285/teachers/envQbertNoFrameskip-v4_20211206-021759_n_iters40000000
